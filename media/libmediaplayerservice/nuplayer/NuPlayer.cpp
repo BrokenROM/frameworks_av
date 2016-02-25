ed1eeb0f853152d0e4454a06cb8c028cbfdeb9b4
Dhananjay Kumar <dhakumar@codeaurora.org>
2016-02-25 13:48:33 +0530

    nuplayer: ignore openAudioSink while waiting for teardown completion
    - AudioSink is reopened on AudioTearDown event as well as on
      decoder output format change. These events are from different
      contexts, independent of each other and can occur concurrently,
      doing set of thread-unsafe operations on audioSink, which
      sometimes may result in fatal error.
    
    - Any request to openSink, after teardown event is received but
      before its completed, is redundant since once teardown is
      completed sink is anyway going to be opened again. Also
      ignoring sink reopen while in teardown would avoid concurrent
      processing of thread-unsafe operations on audioSink.
    
    - Update nuplayer to prevent reopening of sink while teardown is
      still going on.
    
    CRs-Fixed: 978013
    Change-Id: Ieb3825f133c2099e07d6e92ef101dd5156af8a20

--- media/libmediaplayerservice/nuplayer/NuPlayer.cpp
+++ media/libmediaplayerservice/nuplayer/NuPlayer.cpp
@@ -1146,6 +1146,7 @@ void NuPlayer::onMessageReceived(const sp<AMessage> &msg) {
                     mRenderer->flush(
                             false /* audio */, false /* notifyComplete */);
                 }
+                mRenderer->signalAudioTearDownComplete();
 
                 int64_t positionUs;
                 if (!msg->findInt64("positionUs", &positionUs)) {
